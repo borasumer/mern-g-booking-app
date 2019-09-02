3c6559ba026da147b9483b2ac6c03be29b56f15b
Bora Sumer <solasguitarra@hotmail.com>
2019-09-01 22:10:24 -0400

    fetch url has been updated for the production version

--- frontend/src/pages/Auth.jsx
+++ frontend/src/pages/Auth.jsx
@@ -56,7 +56,7 @@ class AuthPage extends Component {
       };
     }
 
-    fetch('http://localhost:8000/graphql', {
+    fetch('https://event-booking-app-merng.herokuapp.com/graphql', {
       method: 'POST',
       body: JSON.stringify(requestBody),
       headers: {
